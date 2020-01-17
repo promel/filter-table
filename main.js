6dbdeb89b41951aa8973d5e990e1c2636a9c6fff
Joseph Kutloano More <jkm.more@gmail.com>
2020-01-17 14:29:57 +0200

    Add files via upload
    Initial upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function search(searchBox) {
+    let input = searchBox;
+    let filter = input.value.toUpperCase();
+    let table = document.querySelector("#myTable")
+    let rows = table.rows;
+    // return;
+    for (let row of rows) {
+        let isMatch = false;
+        for(let cell of row.cells){
+            if (cell && cell.nodeName != 'TH') {
+                let txtValue = cell.textContent || cell.innerText;
+                isMatch = txtValue.toUpperCase().indexOf(filter) > -1;
+                if(isMatch)break;
+            }
+        }
+        row.style.display = isMatch?"":"none";
+    }
+}
