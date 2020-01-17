56c103cd7d2425b66db50a76890084051290d403
Joseph Kutloano More <jkm.more@gmail.com>
2020-01-17 14:38:06 +0200

    Add files via upload
    removed comment

--- main.js
+++ main.js
@@ -2,9 +2,7 @@ function search(searchBox) {
     let input = searchBox;
     let filter = input.value.toUpperCase();
     let table = document.querySelector("#myTable")
-    let rows = table.rows;
-    // return;
-    for (let row of rows) {
+    for (let row of table.rows) {
         let isMatch = false;
         for(let cell of row.cells){
             if (cell && cell.nodeName != 'TH') {
